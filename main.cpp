9a83909c454a4cb9b9f2751f4a654d432a99c40c
Akib558 <xenonsia1234@gmail.com>
2022-11-03 23:14:32 +0600

    road and inside stairs added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88155ee0d1d62d40fd750c194d96d2fc861795bca5fa4df4de895ad111117a39
-size 26298
+oid sha256:5f03ec8367e7eee9542e327f2721af361c03ecd0c6708f900802bfcacf874fd3
+size 29972
