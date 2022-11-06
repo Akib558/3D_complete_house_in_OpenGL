a6904c2629339a9b7db4ef43daff8d7a5db3e351
Akib558 <xenonsia1234@gmail.com>
2022-11-06 22:42:42 +0600

    washroom and reading table added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d546a97ad04461f603a276a5e7a5c7e6c312f77d7cd10a1ed94af3f31dab892
-size 35632
+oid sha256:3c8a89d52b5b08d6063237c2d4487012d8719c2cc71f0168a47fb14eb367942c
+size 39730
